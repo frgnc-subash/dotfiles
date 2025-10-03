976689b09bfbc7932ffa9456f35915a9cb426604
frgnc-subash <axosis.social357@gmail.com>
2025-10-03 17:01:37 +0545

    updates

--- .config/waybar/scripts/reload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pkill waybar
-
-waybar &
-
-hyprctl reload
-
--- .config/waybar/scripts/toggle-date.sh
+++ .config/waybar/scripts/toggle-date.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Toggle file location
+
 TOGGLE_FILE="/tmp/waybar-clock-toggle"
 
-# Check if toggle file exists
+
 if [ -f "$TOGGLE_FILE" ]; then
-    # File exists - remove it (switch to clock mode)
+    
     rm "$TOGGLE_FILE"
 else
-    # File doesn't exist - create it (switch to date mode)
+    
     touch "$TOGGLE_FILE"
 fi
 
-# Signal Waybar to update the clock module
+
 pkill -SIGRTMIN+8 waybar
