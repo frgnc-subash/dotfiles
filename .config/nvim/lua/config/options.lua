f16b626b1442441f84eec0f8e0c68a9a7047489f
frgnc-subash <axosis.social357@gmail.com>
2025-10-10 10:35:49 +0545

    ricing neovim with lazy

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,3 +4,10 @@
 
 vim.g.autoformat = true
 vim.opt.guicursor = ""
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.g.autoformat = true
+vim.opt.guicursor = ""
+
