4b73645b817666f3603027c1154ee629cab9bb59
frgnc-subash <axosis.social357@gmail.com>
2025-10-29 16:42:01 +0545

    moonfly colorsceheme and small update

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,22 +1,22 @@
 return {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-        bigfile = {
-            enabled = true
-        },
-        dashboard = {
-            enabled = true,
-            width = 60,
-            row = nil,
-            col = nil,
-            pane_gap = 4,
-            autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = {
+      enabled = true,
+    },
+    dashboard = {
+      enabled = true,
+      width = 60,
+      row = nil,
+      col = nil,
+      pane_gap = 4,
+      autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
 
-            preset = {
-                pick = nil,
-                header = [[
+      preset = {
+        pick = nil,
+        header = [[
                                                  ███            
                                                 ░░░             
   █████   ██████  ████████   ██████  ████████   ████  ████████  
@@ -27,140 +27,159 @@ return {
 ░░░░░░   ░░░░░░  ░░░░░      ░░░░░░  ░░░░ ░░░░░ ░░░░░ ░░░░ ░░░░░ 
                 ]],
 
-                keys = {{
-                    icon = "󰈞 ",
-                    key = "f",
-                    desc = "Find File",
-                    action = ":lua Snacks.dashboard.pick('files')"
-                }, {
-                    icon = " ",
-                    key = "n",
-                    desc = "New File",
-                    action = ":ene | startinsert"
-                }, {
-                    icon = "󰭷 ",
-                    key = "g",
-                    desc = "Find Text",
-                    action = ":lua Snacks.dashboard.pick('live_grep')"
-                }, {
-                    icon = " ",
-                    key = "r",
-                    desc = "Recent Files",
-                    action = ":lua Snacks.dashboard.pick('oldfiles')"
-                }, {
-                    icon = " ",
-                    key = "c",
-                    desc = "Config",
-                    action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})"
-                }, {
-                    icon = "󱍸 ",
-                    key = "s",
-                    desc = "Restore Session",
-                    section = "session"
-                }, {
-                    icon = "󰒲 ",
-                    key = "l",
-                    desc = "Lazy",
-                    action = ":Lazy",
-                    enabled = package.loaded.lazy ~= nil
-                }, {
-                    icon = " ",
-                    key = "x",
-                    desc = "Lazy Extras",
-                    action = ":LazyExtras",
-                    enabled = package.loaded.lazy ~= nil
-                }, {
-                    icon = " ",
-                    key = "q",
-                    desc = "Quit",
-                    action = ":qa"
-                }}
-            },
+        keys = {
+          {
+            icon = "󰈞 ",
+            key = "f",
+            desc = "Find File",
+            action = ":lua Snacks.dashboard.pick('files')",
+          },
+          {
+            icon = " ",
+            key = "n",
+            desc = "New File",
+            action = ":ene | startinsert",
+          },
+          {
+            icon = "󰭷 ",
+            key = "g",
+            desc = "Find Text",
+            action = ":lua Snacks.dashboard.pick('live_grep')",
+          },
+          {
+            icon = " ",
+            key = "r",
+            desc = "Recent Files",
+            action = ":lua Snacks.dashboard.pick('oldfiles')",
+          },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
+          {
+            icon = "󱍸 ",
+            key = "s",
+            desc = "Restore Session",
+            section = "session",
+          },
+          {
+            icon = "󰒲 ",
+            key = "l",
+            desc = "Lazy",
+            action = ":Lazy",
+            enabled = package.loaded.lazy ~= nil,
+          },
+          {
+            icon = " ",
+            key = "x",
+            desc = "Lazy Extras",
+            action = ":LazyExtras",
+            enabled = package.loaded.lazy ~= nil,
+          },
+          {
+            icon = " ",
+            key = "q",
+            desc = "Quit",
+            action = ":qa",
+          },
+        },
+      },
 
-            formats = {
-                icon = function(item)
-                    if item.file and (item.icon == "file" or item.icon == "directory") then
-                        return M.icon(item.file, item.icon)
-                    end
-                    return {
-                        item.icon,
-                        width = 2,
-                        hl = "icon"
-                    }
-                end,
-                footer = {
-                    "%s",
-                    align = "center"
+      formats = {
+        icon = function(item)
+          if item.file and (item.icon == "file" or item.icon == "directory") then
+            return M.icon(item.file, item.icon)
+          end
+          return {
+            item.icon,
+            width = 2,
+            hl = "icon",
+          }
+        end,
+        footer = {
+          "%s",
+          align = "center",
+        },
+        header = {
+          "%s",
+          align = "center",
+        },
+        file = function(item, ctx)
+          local fname = vim.fn.fnamemodify(item.file, ":~")
+          fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
+          if #fname > ctx.width then
+            local dir = vim.fn.fnamemodify(fname, ":h")
+            local file = vim.fn.fnamemodify(fname, ":t")
+            if dir and file then
+              file = file:sub(-(ctx.width - #dir - 2))
+              fname = dir .. "/…" .. file
+            end
+          end
+          local dir, file = fname:match("^(.*)/(.+)$")
+          return dir
+              and {
+                {
+                  dir .. "/",
+                  hl = "dir",
                 },
-                header = {
-                    "%s",
-                    align = "center"
+                {
+                  file,
+                  hl = "file",
                 },
-                file = function(item, ctx)
-                    local fname = vim.fn.fnamemodify(item.file, ":~")
-                    fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
-                    if #fname > ctx.width then
-                        local dir = vim.fn.fnamemodify(fname, ":h")
-                        local file = vim.fn.fnamemodify(fname, ":t")
-                        if dir and file then
-                            file = file:sub(-(ctx.width - #dir - 2))
-                            fname = dir .. "/…" .. file
-                        end
-                    end
-                    local dir, file = fname:match("^(.*)/(.+)$")
-                    return dir and {{
-                        dir .. "/",
-                        hl = "dir"
-                    }, {
-                        file,
-                        hl = "file"
-                    }} or {{
-                        fname,
-                        hl = "file"
-                    }}
-                end
-            },
+              }
+            or { {
+              fname,
+              hl = "file",
+            } }
+        end,
+      },
 
-            sections = {{
-                section = "header"
-            }, {
-                section = "keys",
-                gap = 1,
-                padding = 1
-            }, {
-                section = "startup"
-            }}
+      sections = {
+        {
+          section = "header",
         },
+        {
+          section = "keys",
+          gap = 1,
+          padding = 1,
+        },
+        {
+          section = "startup",
+        },
+      },
+    },
 
-        explorer = {
-            enabled = true
-        },
-        indent = {
-            enabled = true
-        },
-        input = {
-            enabled = true
-        },
-        picker = {
-            enabled = true
-        },
-        notifier = {
-            enabled = true
-        },
-        quickfile = {
-            enabled = true
-        },
-        scope = {
-            enabled = true
-        },
-        scroll = {
-            enabled = true
-        },
-        statuscolumn = {
-            enabled = true
-        },
-        words = {
-            enabled = true
-        }
-    }
+    explorer = {
+      enabled = true,
+    },
+    indent = {
+      enabled = true,
+    },
+    input = {
+      enabled = true,
+    },
+    picker = {
+      enabled = true,
+    },
+    notifier = {
+      enabled = true,
+    },
+    quickfile = {
+      enabled = true,
+    },
+    scope = {
+      enabled = true,
+    },
+    scroll = {
+      enabled = true,
+    },
+    statuscolumn = {
+      enabled = true,
+    },
+    words = {
+      enabled = true,
+    },
+  },
 }
