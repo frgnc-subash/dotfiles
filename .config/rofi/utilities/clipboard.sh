1db7ed868b1ddc7271fa68a44cea032e6547f194
frgnc-subash <axosis.social357@gmail.com>
2025-11-02 22:00:58 +0545

    mini update

--- .config/rofi/utilities/clipboard.sh
+++ .config/rofi/utilities/clipboard.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# clipmenu.sh - Clipboard history menu with "Clear Clipboard" button
 
 NUM_ENTRIES=10 # number of entries to show
 
@@ -8,17 +7,17 @@ mapfile -t HISTORY < <(cliphist list | tac | tail -n "$NUM_ENTRIES")
 
 # Add "Clear Clipboard" option at the top
 MENU=()
-MENU+=("🗑️ Clear Clipboard")
+MENU+=("󰅎 Clear Clipboard")
 MENU+=("${HISTORY[@]}")
 
 # Show menu in rofi and get selected line
-SELECTED=$(printf '%s\n' "${MENU[@]}" | rofi -dmenu -p "Clipboard History:")
+SELECTED=$(printf '%s\n' "${MENU[@]}" | rofi -dmenu -p " Clipboard :")
 
 # If nothing selected, exit
 [ -z "$SELECTED" ] && exit
 
 # Handle Clear Clipboard selection
-if [ "$SELECTED" = "🗑️ Clear Clipboard" ]; then
+if [ "$SELECTED" = "󰅎 Clear Clipboard" ]; then
     cliphist wipe     # clear history database
     echo -n | wl-copy # clear current clipboard
     exit
