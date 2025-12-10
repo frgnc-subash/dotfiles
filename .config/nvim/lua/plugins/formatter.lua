ee08a458b718bcbbb6984789e875299fc2efdad0
frgnc-subash <axosis.social357@gmail.com>
2025-12-10 06:57:12 +0545

    feat | auto-save neovim plug & ui changes on kitty

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -21,21 +21,4 @@ return {
       sh = { "shfmt" },
     },
   },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = { eslint = {} },
-      setup = {
-        eslint = function()
-          require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "eslint" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
-        end,
-      },
-    },
-  },
 }
