10eee7e1c5f8cf0b25397ad0ce6dab7bf53aad36
frgnc-subash <axosis.social357@gmail.com>
2026-01-01 16:18:17 +0545

    feat | better cava script and mpris

--- .config/waybar/scripts/cava.sh
+++ .config/waybar/scripts/cava.sh
@@ -1,29 +1,30 @@
 #! /bin/bash
 
 bar="▁▂▃▄▅▆▇█"
-dict="s/;//g;"
-
-# creating "dictionary" to replace char with bar
-i=0
-while [ $i -lt ${#bar} ]; do
-    dict="${dict}s/$i/${bar:$i:1}/g;"
-    i=$((i = i + 1))
-done
-
-# write cava config
 config_file="/tmp/waybar_cava_config"
+
 echo "
 [general]
-bars = 14
+bars = 12
 
 [output]
 method = raw
 raw_target = /dev/stdout
 data_format = ascii
 ascii_max_range = 7
-" >$config_file
+" >"$config_file"
 
-# read stdout from cava
-cava -p $config_file | while read -r line; do
-    echo $line | sed $dict
+trap 'rm -f $config_file' EXIT
+
+cava -p "$config_file" | while read -r line; do
+    formatted="${line//;/}"
+    output=""
+
+    len=${#formatted}
+    for ((i = 0; i < len; i++)); do
+        digit=${formatted:$i:1}
+        output="${output}${bar:$digit:1}"
+    done
+
+    echo "$output"
 done
