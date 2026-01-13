17595dbae2210c1dfda35482c01ca6aaad23ee38
frgnc-subash <axosis.social357@gmail.com>
2026-01-13 16:59:08 +0545

    feat | better waybar reload script and user changes

--- .config/hypr/scripts/reload.sh
+++ .config/hypr/scripts/reload.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-pkill waybar
+killall -q waybar
+pkill -f "cava -p /tmp/waybar_cava_config"
+
+while pgrep -x waybar >/dev/null || pgrep -f "cava -p /tmp/waybar_cava_config" >/dev/null; do
+    sleep 0.1
+done
 
 waybar &
+disown
 
 hyprctl reload
