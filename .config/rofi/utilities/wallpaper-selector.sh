ea897abc6f6268bf7e1f1036a82f38f361e848e0
frgnc-subash <axosis.social357@gmail.com>
2026-01-15 20:52:17 +0545

    feat | update user settings

--- .config/rofi/utilities/wallpaper-selector.sh
+++ .config/rofi/utilities/wallpaper-selector.sh
@@ -4,7 +4,6 @@ WALLPAPER_BASE="$HOME/Pictures/wallpapers"
 HYPR_THEME_FILE="$HOME/.config/hypr/theme.conf"
 ROFI_CONFIG="$HOME/.config/rofi/utilities/wallpaper-selector.rasi"
 CACHE_DIR="$HOME/.cache/wallpaper-thumbnails"
-STATE_FILE="$HOME/.cache/wallpaper-cycle-index"
 
 CURRENT_SOURCE=$(grep "source =" "$HYPR_THEME_FILE" | awk '{print $3}')
 if [[ "$CURRENT_SOURCE" == *"matugen"* ]]; then
@@ -39,20 +38,14 @@ if [ "$MODE" == "gui" ]; then
         done | rofi -dmenu -i -show-icons -p "󰸉" -theme "$ROFI_CONFIG"
     )
 elif [ "$MODE" == "cycle" ]; then
-    mapfile -t WALLS < <(find "$TARGET_DIR" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.webp" \) | sort)
-    COUNT=${#WALLS[@]}
-    [ "$COUNT" -eq 0 ] && exit 1
-
-    INDEX=$(cat "$STATE_FILE" 2>/dev/null || echo 0)
-    SELECTED_PATH="${WALLS[$INDEX]}"
+    SELECTED_PATH=$(find "$TARGET_DIR" -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.webp" \) | shuf -n 1)
+    [ -z "$SELECTED_PATH" ] && exit 1
     SELECTED_FILE=$(basename "$SELECTED_PATH")
-
-    echo "$(((INDEX + 1) % COUNT))" >"$STATE_FILE"
 fi
 
 if [ -n "$SELECTED_FILE" ]; then
     FULL_PATH="$TARGET_DIR/$SELECTED_FILE"
     swww img "$FULL_PATH" --transition-type any --transition-duration 1.5 --transition-fps 90
+
     [ "$THEME_MODE" == "dynamic" ] && matugen image "$FULL_PATH"
 fi
-
