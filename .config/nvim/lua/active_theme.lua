5318684b1e26ddf58c2d547bb52c50fc9fdcf283
frgnc-subash <axosis.social357@gmail.com>
2026-01-15 13:39:13 +0545

    feat | added themes for nvim

--- .config/nvim/lua/active_theme.lua
+++ .config/nvim/lua/active_theme.lua
@@ -1,12 +1,10 @@
-return {
-  {
+return {{
     "bluz71/vim-moonfly-colors",
     name = "moonfly",
     lazy = false,
     priority = 1000,
     config = function()
-      vim.g.moonflyItalics = true
-      vim.cmd.colorscheme("moonfly")
-    end,
-  },
-}
+        vim.g.moonflyItalics = true
+        vim.cmd.colorscheme("moonfly")
+    end
+}}
